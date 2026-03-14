0814fee95150b41a67a5dbd4097b3a1f3ff476e2
hotaru-14 <hotaru1495@gmail.com>
2026-03-14 17:33:41 +0900

    fix: 開発と本番のDBの違いに考慮

--- app.py
+++ app.py
@@ -5,21 +5,29 @@ from fastapi import FastAPI
 from google.cloud.sql.connector import Connector, IPTypes
 
 app = FastAPI()
-connector = Connector(refresh_strategy="LAZY")
 
 
-def getconn():
-    return connector.connect(
-        os.environ["INSTANCE_CONNECTION_NAME"],
-        "pg8000",
-        user=os.environ["DB_USER"],
-        password=os.environ["DB_PASS"],
-        db=os.environ["DB_NAME"],
-        ip_type=IPTypes.PUBLIC,
-    )
+def create_engine():
+    database_url = os.getenv("DATABASE_URL")
+    if database_url:
+        return sqlalchemy.create_engine(database_url)
+
+    connector = Connector(refresh_strategy="LAZY")
+
+    def getconn():
+        return connector.connect(
+            os.environ["INSTANCE_CONNECTION_NAME"],
+            "pg8000",
+            user=os.environ["DB_USER"],
+            password=os.environ["DB_PASS"],
+            db=os.environ["DB_NAME"],
+            ip_type=IPTypes.PUBLIC,
+        )
+
+    return sqlalchemy.create_engine("postgresql+pg8000://", creator=getconn)
 
 
-engine = sqlalchemy.create_engine("postgresql+pg8000://", creator=getconn)
+engine = create_engine()
 
 
 @app.get("/")
@@ -35,4 +43,3 @@ def get_users():
         ).mappings().all()
 
     return {"items": [dict(row) for row in rows]}
-
