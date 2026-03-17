c021a5629c7f095a79a544b287f2a3fdab558416
hotaru-14 <hotaru1495@gmail.com>
2026-03-17 15:18:17 +0900

    feat: api/result/saveの入出力の型定義追加

--- schemas/result.py
+++ schemas/result.py
@@ -14,3 +14,17 @@ class GenerateResultResponse(BaseModel):
     tf_score: int
     jp_score: int
     summary: str
+
+
+class InsertResultSummaryRequest(GenerateResultResponse):
+    diaries_id: int
+
+class InsertResultSummaryResponse(BaseModel):
+    id: int
+    diaries_id: int
+    type: str
+    ei_score: int
+    sn_score: int
+    tf_score: int
+    jp_score: int
+    summary: str
