779a58ea3bafbb67308db42f996b979ead00a37c
hotaru-14 <hotaru1495@gmail.com>
2026-03-19 15:40:54 +0900

    fix: パスパラメータとクエリパラメータの変数指定間違いを修正

--- api/routers/common.py
+++ api/routers/common.py
@@ -6,10 +6,10 @@ from fastapi import Depends
 
 router = APIRouter()
 
-@router.get("/api/diaries/{diary_id}/diary-questions-answer", response_model=GetFullDiaryDataResponse)
-def get_full_diary_data(diaries_id: int, current_user: dict = Depends(get_current_user)):
+@router.get("/api/diaries/{diary_id}/diary-questions-answers", response_model=GetFullDiaryDataResponse)
+def get_full_diary_data(diary_id: int, current_user: dict = Depends(get_current_user)):
     try:
-        return get_full_diary_data_service(current_user["user_id"], diaries_id)
+        return get_full_diary_data_service(current_user["user_id"], diary_id)
     except HTTPException:
         raise
     except Exception as e:
