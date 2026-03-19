7be92b258c0fcf5b0e07f1268daa190737cffe53
kamo-g-2 <k1k12kamokeigo@icloud.com>
2026-03-19 17:11:29 +0900

    feat: 日記とタイプを日付が新しい順で全取得するapiのルーターを実装

--- api/routers/common.py
+++ api/routers/common.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException
-from schemas.common import GetFullDiaryDataResponse
-from services.common import get_full_diary_data_service
+from schemas.common import GetFullDiaryDataResponse, GetFullTypesResponse
+from services.common import get_full_diary_data_service, get_full_types_service
 from core.auth import get_current_user
 from fastapi import Depends
 
@@ -15,3 +15,11 @@ def get_full_diary_data(diaries_id: int, current_user: dict = Depends(get_curren
     except Exception as e:
         print(f"Error: {e}")
         raise HTTPException(status_code=500, detail="データの一括取得に失敗しました")
+
+@router.get("/api/diaries/type", response_model= list[GetFullTypesResponse])
+def get_all_types(current_user: dict = Depends(get_current_user)):
+    try:
+        return get_full_types_service(user_id=current_user["user_id"])
+    except Exception as e:
+        print(f"Error: {e}")
+        raise HTTPException(status_code=500, detail="データの取得に失敗しました")
