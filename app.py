b6cadaeee5cdefa67057e871416a58d8c50a5a0d
kamo-g-2 <k1k12kamokeigo@icloud.com>
2026-03-19 11:17:14 +0900

    feat: 日記、質問、回答を取得するapiのルーターを追加

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from api.routers import diaries
 from api.routers import questions
 from api.routers import answers
 from api.routers import result
+from api.routers import common
 from core.db import engine
 
 app = FastAPI()
@@ -12,6 +13,7 @@ app.include_router(diaries.router)
 app.include_router(questions.router)
 app.include_router(answers.router)
 app.include_router(result.router)
+app.include_router(common.router)
 
 @app.get("/")
 def root():
