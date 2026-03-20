ddb9b938ea3cd2837f674cd072b2d65751e7d6eb
hotaru-14 <hotaru1495@gmail.com>
2026-03-20 18:15:39 +0900

    restore from development

--- services/common.py
+++ services/common.py
@@ -1,4 +1,4 @@
-from schemas.common import GetFullDiaryDataResponse, DiarySection,QuestionsWrapper,QuestionDetail,AnswersWrapper,AnswerDetail
+from schemas.common import GetFullDiaryDataResponse, DiarySection,QuestionsWrapper,QuestionDetail,AnswersWrapper,AnswerDetail, GetFullTypesResponse
 from fastapi import HTTPException
 from core.db import engine
 import sqlalchemy
@@ -37,3 +37,21 @@ def get_full_diary_data_service(user_id: int, diaries_id:int) -> GetFullDiaryDat
         questions=QuestionsWrapper(questions=result["questions"] or []),
         answers=AnswersWrapper(answers=result["answers"] or [])
     )
+
+def get_full_types_service(user_id: int) -> list[GetFullTypesResponse]:
+    with engine.connect() as conn:
+        sql = sqlalchemy.text("""
+            SELECT
+                d.body,
+                TO_CHAR(d.created_at, 'YYYY-MM-DD') as date,
+                TO_CHAR(d.created_at, 'HH24:MI') as time,
+                r.type
+            FROM diaries d
+            INNER JOIN results r ON d.id = r.diaries_id
+            WHERE d.user_id = :user_id
+            ORDER BY d.created_at DESC
+        """)
+
+        rows = conn.execute(sql, {"user_id": user_id}).mappings().all()
+    
+    return [GetFullTypesResponse(**row) for row in rows]
